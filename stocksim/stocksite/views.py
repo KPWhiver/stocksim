3d8d3d914cf9a2d72ff624e023e547728f6d3715
BlizzardFenrir <jan.veldthuis@gmail.com>
2013-09-18 11:36:21 +0200

    Added static file support.

--- stocksim/stocksite/views.py
+++ stocksim/stocksite/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 import datetime
 
 def home(request):
-    
-    
-    
-    return render_to_response('home.html', {'string': 'hello world'})
+    return render_to_response('home.html', {'string': 'hello world'}, context_instance=RequestContext(request))
