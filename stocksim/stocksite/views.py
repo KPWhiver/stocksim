53a1700de85c951636ceea6d23cca4afe8eccfe2
s1983377 <s1983377@fwn-bborg-edu-74-94.fmnsedu.rug.nl>
2013-10-02 10:42:47 +0200

    Added proper feed reading from yahoo

--- stocksim/stocksite/views.py
+++ stocksim/stocksite/views.py
@@ -7,17 +7,30 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
+import feedparser
+
 # Local imports
 from stocksite.models import Company, History, TimePoint, UserProfile
 
 @login_required
 def home(request):   
+    d = feedparser.parse('http://finance.yahoo.com/news/?format=rss')
+    numOfItems = min(10, len(d.entries))
+    
+    news = []
+    
+    for entry in d.entries[0:numOfItems]:
+      dateParsed = entry.published_parsed
+      date = datetime.datetime(dateParsed.tm_year, dateParsed.tm_mon, dateParsed.tm_mday, dateParsed.tm_hour, dateParsed.tm_min)
+    
+      news += [entry.title, entry.link, date]
+    """
     news = [['Klaas crowned world emperor', '#', datetime.date(2013, 10, 1)], 
             ['Resistance to Klaasolution slowly ceases', '#', datetime.date(2013, 9, 29)], 
             ['Klaasolution has started, millions march the streets to overthrow governments', '#', datetime.date(2013, 9, 26)], 
             ['Klaas proclaims himself world leader, calls on overthrowing governments', '#', datetime.date(2013, 9, 22)], 
             ['Normal day, no news', '#', datetime.date(2013, 9, 13)]]    
-    
+    """
     return render(request, 'home.html', {'news': news})
     
 def companies(request):
