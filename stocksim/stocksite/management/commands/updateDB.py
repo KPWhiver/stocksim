fbdca0503c7241b0024cbf632a551bccfb339be5
Stephan AWS <stephangroenewold2@hotmail.com>
2013-10-15 08:42:00 +0000

    Added commands to manage.py for calling methods in the backend

---
+++ stocksim/stocksite/management/commands/updateDB.py
@@ -0,0 +1,46 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
+import backend
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--companies',
+            action='store_true',
+            dest='companies',
+            default=False,
+            help='Add companies to the database'),
+        make_option('--fill',
+            action='store_true',
+            dest='fill',
+            default=False,
+            help='Fill the database with entries for the companies currently in the database'),
+        make_option('--daily',
+            action='store_true',
+            dest='daily',
+            default=False,
+            help='Update the daily data in the database'),
+        make_option('--historical',
+            action='store_true',
+            dest='historical',
+            default=False,
+            help='Update the historical data in the database'),
+        )
+
+    def handle(self, *args, **options):
+        if options['companies']:
+          num = 1
+          if len(args) > 0:
+            try:
+              num = int(args[0])
+            except:
+              raise CommandError('Invalid companies interval')
+          
+          backend.addCompanies(num)
+        elif options['fill']:
+          backend.fillDatabase()
+        elif options['daily']:
+          backend.updateDailyData()
+        elif options['historical']:
+          backend.updateHistoricData()
+        else:
+          raise CommandError('Missing flag')
