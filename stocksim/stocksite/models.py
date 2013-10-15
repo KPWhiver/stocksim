4e4d1ac5a8383d4d5b51948e4e6b6a519d1451d6
Klaas <klaaswinter@gmail.com>
2013-10-15 15:57:34 +0200

    Partially added map reduce for total worth

--- stocksim/stocksite/models.py
+++ stocksim/stocksite/models.py
@@ -8,6 +8,8 @@ from django_mongodb_engine.contrib import MongoDBManager
 
 import datetime
 
+from bson.son import SON
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     money = models.DecimalField(max_digits=50, decimal_places=4, default=0)
@@ -63,6 +65,8 @@ class Company(models.Model):
     longName = models.CharField(max_length=50) # Please insert appropriate max_length
     historicData = ListField(EmbeddedModelField('History'))
     dailyData = ListField(EmbeddedModelField('TimePoint'))
+    
+    objects = MongoDBManager()
 
 class History(models.Model):
     date = models.DateField(default=datetime.date.today)
@@ -108,6 +112,77 @@ def create_profile(sender, instance, created, **kwargs):
 class StockCount(models.Model):
     company = models.ForeignKey(Company)
     amount = models.BigIntegerField()
+
+def totalWorth():
+    mapfuncUser = """
+    function() 
+    {
+        var id = this._id;
+        this.stocks.forEach(function(stock) 
+        {
+          var perUser = {amount: stock.amount, userid: id}; 
+          emit(stock.company_id, {owners: [perUser], price: 0});
+        }
+        )
+    }
+    """    
+    mapfuncCompany = """
+    function() 
+    { 
+        var data = this.historicData[this.historicData.length - 1];
+        if(data !== undefined)
+        {
+          emit(this._id, {owners: [], price: data.closePrice});
+        }
+        else
+        {
+          emit(this._id, {owners: [], price: 0});
+        }
+        
+    }
+    """    
+    reducefunc = """
+    function reduce(key, values) 
+    {
+        var result = {owners: [], price: 0};
+        
+        values.forEach(function(value) 
+        {
+            if(value.owners !== [])
+            {
+              result.owners.concat(value.owners); 
+            }
+            
+            if(result.price === 0 && value.price !== 0)
+            {
+              result.price = value.price;
+            }
+        });
+        
+        return result;
+    }
+    """
+    res = UserProfile.objects.map_reduce(mapfuncUser, reducefunc, out={'reduce': 'temp_worth'})
+    res = Company.objects.map_reduce(mapfuncCompany, reducefunc, out={'reduce': 'temp_worth'})
+    
+    
+    for pair in res:
+        print pair
+        
+        
+    
+        """
+        comp = Company.objects.get(id=pair.key)
+        count = None
+
+        if StockCount.objects.filter(company = comp).exists():
+            count = StockCount.objects.get(company = comp)
+            count.amount = pair.value
+        else:
+            count = StockCount(company=comp, amount=pair.value)
+
+        count.save()
+        """
 
 def totalStockBought():
     mapfunc = """
