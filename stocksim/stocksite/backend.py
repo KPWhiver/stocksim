ef7c7dacd719cf53c20fd6b7f23889faa6754e3e
Klaas <klaaswinter@gmail.com>
2013-10-15 12:23:11 +0200

    Added function to get 30 days historic data

--- stocksim/stocksite/backend.py
+++ stocksim/stocksite/backend.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+import datetime
 import time
 import csv
 
 try:
     # py3
-    from urllib.request import Request, urlopen
+    from urllib.request import Request, urlopen, HTTPError
     from urllib.parse import urlencode
 except ImportError:
     # py2
-    from urllib2 import Request, urlopen
+    from urllib2 import Request, urlopen, HTTPError
     from urllib import urlencode
     
 from stocksite.models import Company, History, TimePoint
@@ -38,8 +38,6 @@ def getHistoricalPrices(symbol, startDate, endDate):
         'g': 'd',
         'ignore': '.csv',
     })
-    
-    print params
     url = 'http://ichart.yahoo.com/table.csv?%s' % params
     req = Request(url)
     resp = urlopen(req)
@@ -66,11 +64,12 @@ def updateHistoricData():
     companies = Company.objects.all()
 
     for company in companies:
-        today = datetime.today()
+        today = datetime.datetime.today()
         data = getHistoricalPrices(company.shortName, today, today)
         data = data.values()[0]
         company.historicData.append(History(volume = data['Volume'], adjustedClosePrice = data['Adj Close'], highPrice = data['High'], lowPrice = data['Low'], closePrice = data['Close'], openPrice = data['Open']))
         company.save()
+        time.sleep(5)
    
 def updateDailyDataRange(companies, startIndex, length):
     companyString = ''
@@ -136,11 +135,24 @@ def fillDatabase():
     companies = Company.objects.all()
 
     for company in companies:
-        yesterday = datetime.today() - datetime.timedelta(1)
-        monthago = today - datetime.timedelta(30)
-        data = getHistoricalPrices(company.shortName, monthago, yesterday)
-        #data = data.values()[0]
-        #company.historicData.append(History(volume = data['Volume'], adjustedClosePrice = data['Adj Close'], highPrice = data['High'], lowPrice = data['Low'], closePrice = data['Close'], openPrice = data['Open']))
-        #company.save()
+        company.historicData = []
+        print company.shortName
+        
+        yesterday = datetime.datetime.today() - datetime.timedelta(1)
+        monthago = yesterday - datetime.timedelta(30)
+        data = {}
+        try:
+            data = getHistoricalPrices(company.shortName, monthago, yesterday)
+        except HTTPError, e:
+            print 'Error occurred:', e, 'company:', company.shortName
+            continue
+            
+        for (date, values) in data.items():
+            day = datetime.date(int(date[0:4]), int(date[5:7]), int(date[8:10]))
+            company.historicData.append(History(date = date, volume = values['Volume'], adjustedClosePrice = values['Adj Close'], 
+                                        highPrice = values['High'], lowPrice = values['Low'], closePrice = values['Close'], openPrice = values['Open']))
+        
+        company.save()
+        time.sleep(5)
         
 
